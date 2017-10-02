5c48c529abec9bb966eb1fe14e5481dbdcb8dcf6
ctwoolsey <clark.woolsey@gmail.com>
2017-10-02 11:29:56 -0500

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home';
 import { AboutComponent } from './about';
+import { CourtComponent } from './court';
 import { NoContentComponent } from './no-content';
 
 import { DataResolver } from './app.resolver';
@@ -11,5 +12,6 @@ export const ROUTES: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'detail', loadChildren: './+detail#DetailModule'},
   { path: 'barrel', loadChildren: './+barrel#BarrelModule'},
+  { path: 'court', component: CourtComponent },
   { path: '**',    component: NoContentComponent },
 ];
