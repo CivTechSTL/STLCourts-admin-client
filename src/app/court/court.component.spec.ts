f97ddb38fad3ee5da0e8457da5f306d13ed569d8
ctwoolsey <clark.woolsey@gmail.com>
2017-10-19 15:33:58 -0500

    Basic unit test created for base components

--- src/app/court/court.component.spec.ts
+++ src/app/court/court.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA} from '@angular/core';
 
 import { CourtComponent } from './court.component';
 
@@ -8,7 +9,8 @@ describe('CourtComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CourtComponent ]
+      declarations: [ CourtComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
